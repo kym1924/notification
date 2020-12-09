fe741ca6da31b6304c9a68639565a37f8f45898e
kym1924 <kym1924@naver.com>
2020-12-09 22:22:08 +0900

    Add AlarmReceiver (using Calendar)

--- notificationCompat/app/src/main/java/com/notification/compat/MainActivity.kt
+++ notificationCompat/app/src/main/java/com/notification/compat/MainActivity.kt
@@ -5,9 +5,9 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.os.SystemClock
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,10 +17,12 @@ class MainActivity : AppCompatActivity() {
         val alarmManager = this.getSystemService(Context.ALARM_SERVICE) as AlarmManager
 
         btn_notification.setOnClickListener{
-            val time = (SystemClock.elapsedRealtime()+60*1000) // after 1 minute
+            val calendar = Calendar.getInstance()
+            calendar.set(Calendar.HOUR_OF_DAY, 22)
+            calendar.set(Calendar.MINUTE, 0)
             val intent = Intent(this, AlarmReceiver::class.java)
             val pendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
-            alarmManager.set(AlarmManager.RTC_WAKEUP, time, pendingIntent)
+            alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, AlarmManager.INTERVAL_DAY, pendingIntent)
         }
     }
 }
